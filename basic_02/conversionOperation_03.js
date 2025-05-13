44a7de14b6666737420a6e849397b09ead8e2f65
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-05-13 03:10:42 +0000

    conversion of datatypes in js

---
+++ basic_02/conversionOperation_03.js
@@ -0,0 +1,42 @@
+// let score = 22
+// let score ="22"
+// let score = "22abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Hilal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "22" => 22
+// "22abc" => Nan
+// true => 1: false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn ="Hilal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Hilal" => true
+
+let someNumber = 99
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
