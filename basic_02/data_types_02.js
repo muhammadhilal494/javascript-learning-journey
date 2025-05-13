d1b3f9266858f87de969ad1cc7bb076a3838b5d8
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-05-13 02:52:38 +0000

    datatypes in js

---
+++ basic_02/data_types_02.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  // using nodejs, not browser
+
+/* console.log(3
+     +3) console.log("Hilal"); // prefer the code readability
+*/
+
+let name = "hilal"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// number => 2 to power 53 //range
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "hilal");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
