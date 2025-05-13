23c27c2e9d4ed387e4f1e19ab63d993525b71cb1
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-05-13 02:35:11 +0000

    variables in js

---
+++ basic_02/variables_01.js
@@ -0,0 +1,22 @@
+const accountId = 1234123
+let accountEmail = 'muhammadhilal494513@gmail.com'
+var accountPassword = "12345"
+accountCity = "Thall"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hilalahmad494520@gmail.com"
+accountPassword = "98765"
+accountCity = "Hangu"
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var because 
+of issue in block scope and functional scope
+*/
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
