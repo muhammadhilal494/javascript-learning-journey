7cdcacdb5d99cf89367de02ba1b9846cf6e7d45e
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-07-22 15:52:25 +0000

    comparison concept clear

---
+++ basic_02/comparison_04.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1); // here the need of typescripte
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Strict check '==='
+console.log("2" === 2); 
+
+// Clean code is more value
+
+
+
+
+
+
