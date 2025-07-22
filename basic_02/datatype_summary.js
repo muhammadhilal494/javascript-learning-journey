ecd0eecb8d9cc61ae51bd978558212f306e009bf
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-07-22 17:26:05 +0000

    datatype summary explanation

---
+++ basic_02/datatype_summary.js
@@ -0,0 +1,43 @@
+// how data is placed in memory
+// in this base datatypes have two types
+// Primitive datatype and reference/non-premitive datatypes
+
+//Premitive: call by value: copy changes
+// 7 types: String, Number, Boolean, null, underfined, Symbol, Bigint
+// symbol: to make unique value
+
+const score = 100
+const score2 = false
+// const score3:Number = 100 // typescripte
+
+const scoreValue = 100.4
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 36353735227654n
+
+
+
+// Reference (Non primitive) : call by reference
+//JS master: master the object and brower event
+
+// Array, Objects, Functions
+
+const heros = ["sultan", 'khan', "osman"]
+
+let obj = {
+    name: "Hilal",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+ 
+console.log(typeof myFunction)
