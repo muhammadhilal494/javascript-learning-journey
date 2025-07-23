b9c8b82db3c4dc2d3dc55c83d223074d941244c6
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-07-23 14:01:30 +0000

    Number and Maths in JS

---
+++ basic_02/06_number-maths.js
@@ -0,0 +1,56 @@
+const score = 200
+// console.log(score);
+
+const balance = new Number(201)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const size = 324.6743
+
+// console.log(size.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Min, Max and Safe-Value
+
+//++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// Check with inspect
+
+// console.log(Math.abs(-5));//Absolute value
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.8));
+
+//other methods in inspect
+
+// console.log(Math.min(3,6,1,8,4));
+// console.log(Math.max(3,6,1,8,4));
+
+//random value 0-1
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10+1);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
