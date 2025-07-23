e08ff61aeec74c9046540b8a0606644b1123474a
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-07-23 15:59:06 +0000

    More methods of arrays

---
+++ 03_basics/02_arrays.js
@@ -0,0 +1,31 @@
+const heros = ["sultan", "mukhtar", "qasim" ]
+const turk_heros = ["ertughal", "osman", "orhan"]
+
+// heros.push(turk_heros)
+// console.log(heros);
+// console.log(heros[3][2]);
+
+const all_heros = heros.concat(turk_heros)
+// console.log(heros);
+// console.log(all_heros);
+
+// spread operator
+// const all_new_heros = [...heros, ...turk_heros]
+// console.log(all_new_heros);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [8, 9, [0, 0]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("hilal"));
+// console.log(Array.from("hilal"));
+// console.log(Array.from({name: "hilal"}));// interesting
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1, score2, score3));
+
+
+
+
