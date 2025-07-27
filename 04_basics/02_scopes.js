67a616a034f1027698a20aa10ed9fdb2bdbd4902
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-07-27 06:13:40 +0000

    scope level and mini hoisting

--- 04_basics/02_scopes.js
+++ 04_basics/02_scopes.js
@@ -12,7 +12,7 @@ if (true) {
     const b = 20
     // var c = 30
     // c = 30
-    console.log(`Inside block scope: ${a}`);
+    // console.log(`Inside block scope: ${a}`);
     
 
 }
@@ -21,6 +21,48 @@ if (true) {
 //     console.log(`Inside for loop: ${c}`);
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+//Nested scopes
+// function outerFunction() {
+//     const username = 'Hilal';
+
+//     function innerFunction() {
+//         const website = 'hilal.com';
+//         console.log(`Username: ${username}`);
+//     }
+    
+//     // console.log(website); // This will throw an error because 'website' is not defined in this scope
+//     innerFunction(); // Call the inner function to access 'website'
+// }    
+// outerFunction(); // Call the outer function to execute the code
+
+// if (true) {
+//     const username = 'Hilal';
+//     if (true) {
+//         const website = 'hilal.com';
+//         console.log(`Username: ${username}`);
+//     }
+//     // console.log(website); // This will throw an error because 'website' is not defined in this scope
+
+// }
+
+
+//+++++++++++++++ intersection of scopes +++++++++++++++++++
+
+// add(5); // 15, not throwing an error because 'add' is hoisted
+
+// function add(x) {
+//    return x + 10;
+// }
+
+
+addTwo(5); // 7 . // This will throw an error because 'addTwo' is not defined yet
+const addTwo = function(x){
+    return x + 2;
+}
+
+addTwo(5); // 7
