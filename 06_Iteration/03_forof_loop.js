00a08a5348f16b86071408b3b49b9b59ab7daaa1
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-02 09:40:52 +0000

    for...of loop usage with arrays, strings, and maps

---
+++ 06_Iteration/03_forof_loop.js
@@ -0,0 +1,47 @@
+// for of loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);    
+// }
+
+
+// const greetings = "Hello World"
+// for (const greet of greetings) {
+//     console.log(`Eac char is ${greet}`);    
+// }
+
+
+// Maps
+
+// const map = new Map()
+// map.set('PK', 'Pakistan')
+// map.set("AF","Afghanistan")
+// map.set('PK', 'Pakistan')
+// console.log(map);
+
+
+// const map = new Map()
+// map.set('PK', 'Pakistan')
+// map.set("AF","Afghanistan")
+// map.set('PK', 'Pakistan')
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+//Objects are not iterable  in forof loop
+
+// const myObj = {
+//     'game1': "Football",
+//     'game2': "Running"
+// }
+
+// for (const [key, valye] of myObj) {
+//     console.log(key, ':-', value);    
+// }
+
