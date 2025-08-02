c255a6bb6038d44b89a7ccc7ef7d6c4023717aa3
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-02 08:14:00 +0000

    while and do-while loops examples

---
+++ 06_Iteration/02_while_do-while_loop.js
@@ -0,0 +1,30 @@
+// while and do-while loop
+
+// while (conditio) {
+    
+// }
+
+
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let array = ["khan", "sultan", "Badshah"]
+// let arr = 0
+// while (arr < array.length) {
+//     console.log(`Value is ${array[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// let score = 1
+// do{
+//     console.log(`Score is ${score}`);
+//       score ++; 
+// }while (score <= 10) {
+//     console.log(`do while body`);  
+// }
