31e7f9aebbf1aa01820c3be72029161e362b6e3a
Muhammad Hilal <muhammadhilal494513@gmail.com>
2025-08-02 08:12:54 +0000

    Add examples of for loops, including break and continue keywords

---
+++ 06_Iteration/01_for_loop.js
@@ -0,0 +1,58 @@
+// For loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];   
+// }
+
+
+// Ctrl + D : duplication of cursor
+// for (let index = 0; index < 10; index++) {
+//     const element = index; 
+//     if (index == 5) {
+//        console.log("5 is best number");
+        
+//     }
+//     console.log(element);
+// }
+// console.log(element);
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     // console.log(`Outer loop value:   ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         // console.log(`Inner loop value ${j}`);
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+//     const element = i;
+// }
+
+
+// let array = ["khan", "Sultan", "Badshah"]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element); 
+// }
+
+
+// break and continue : keyword
+// for (let index = 0; index < 20; index++) {
+  
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+        
+//     }
+//     console.log(`Value of index is ${index}`);   
+// }
+
+
+// for (let index = 0; index < 20; index++) {
+  
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+        
+//     }
+//     console.log(`Value of index is ${index}`);   
+// }
